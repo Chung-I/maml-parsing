882b586acda62a36989158346f87a64ae7e88905
Chung-I <leon129506@gmail.com>
2020-03-26 17:03:36 +0800

    missed file in transition-based model initial commit

---
+++ src/data/dataset_readers/util.py
@@ -0,0 +1,81 @@
+def _obtain_child_index_for_left2right(heads):
+    child_ids = [[] for _ in range(len(heads))]
+    # skip the symbolic root.
+    for child in range(1, len(heads)):
+        head = heads[child]
+        child_ids[head].append(child)
+    return child_ids
+
+
+def _obtain_child_index_for_inside_out(heads):
+    child_ids = [[] for _ in range(len(heads))]
+    for head in range(len(heads)):
+        # first find left children inside-out
+        for child in reversed(range(1, head)):
+            if heads[child] == head:
+                child_ids[head].append(child)
+        # second find right children inside-out
+        for child in range(head + 1, len(heads)):
+            if heads[child] == head:
+                child_ids[head].append(child)
+    return child_ids
+
+
+def _obtain_child_index_for_depth(heads, reverse):
+    def calc_depth(head):
+        children = child_ids[head]
+        max_depth = 0
+        for child in children:
+            depth = calc_depth(child)
+            child_with_depth[head].append((child, depth))
+            max_depth = max(max_depth, depth + 1)
+        child_with_depth[head] = sorted(child_with_depth[head], key=lambda x: x[1], reverse=reverse)
+        return max_depth
+
+    child_ids = _obtain_child_index_for_left2right(heads)
+    child_with_depth = [[] for _ in range(len(heads))]
+    calc_depth(0)
+    return [[child for child, depth in child_with_depth[head]] for head in range(len(heads))]
+
+def generate_stack_inputs(heads, types, prior_order):
+    if prior_order == 'deep_first':
+        child_ids = _obtain_child_index_for_depth(heads, True)
+    elif prior_order == 'shallow_first':
+        child_ids = _obtain_child_index_for_depth(heads, False)
+    elif prior_order == 'left2right':
+        child_ids = _obtain_child_index_for_left2right(heads)
+    elif prior_order == 'inside_out':
+        child_ids = _obtain_child_index_for_inside_out(heads)
+    else:
+        raise ValueError('Unknown prior order: %s' % prior_order)
+
+    stacked_heads = []
+    children = []
+    siblings = []
+    stacked_types = []
+    skip_connect = []
+    prev = [0 for _ in range(len(heads))]
+    sibs = [0 for _ in range(len(heads))]
+    stack = [0]
+    position = 1
+    while len(stack) > 0:
+        head = stack[-1]
+        stacked_heads.append(head)
+        siblings.append(sibs[head])
+        child_id = child_ids[head]
+        skip_connect.append(prev[head])
+        prev[head] = position
+        if len(child_id) == 0:
+            children.append(head)
+            sibs[head] = 0
+            stacked_types.append('PAD')
+            stack.pop()
+        else:
+            child = child_id.pop(0)
+            children.append(child)
+            sibs[head] = child
+            stack.append(child)
+            stacked_types.append(types[child])
+        position += 1
+
+    return stacked_heads, children, siblings, stacked_types, skip_connect
